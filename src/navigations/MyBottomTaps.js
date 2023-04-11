965cd560ca7010a141eeb10823714e9f867faca1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-04-10 20:52:16 -0500

    added icons

--- src/navigations/MyBottomTaps.js
+++ src/navigations/MyBottomTaps.js
@@ -1,14 +1,40 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
+import {FontAwesome} from '@expo/vector-icons'
 
 const MyTab = createBottomTabNavigator();
 
 export default function MyBottomTab(){
    return(
-   <MyTab.Navigator>
-    <MyTab.Screen name="Home" component={Home}/>
-    <MyTab.Screen name="Profile" component={Profile}/>
+   <MyTab.Navigator
+   initialRouteName='Home'
+   screenOptions={{
+    headerTitleAlign:'center'
+   }}
+   >
+    <MyTab.Screen
+     name="Home" 
+     component={Home}
+     options={{
+        tabBarIcon:({color})=> <FontAwesome 
+                     name="home"
+                     size={24}
+                     color= {color}
+                     />
+     }}
+     />
+    <MyTab.Screen 
+    name="Profile"
+     component={Profile}
+     options={{
+        tabBarIcon:({color})=> <FontAwesome 
+                     name="user"
+                     size={24}
+                     color= {color}
+                     />
+     }}
+     />
    </MyTab.Navigator>
    
    ) 
